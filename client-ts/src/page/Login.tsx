be039fac4926345c8d70a846af88ff1741b5340b
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-02-23 17:13:13 +0700

    change sign in text

--- client-ts/src/page/Login.tsx
+++ client-ts/src/page/Login.tsx
@@ -71,10 +71,10 @@ export default function SignInSide() {
   
                         <div className="d-flex align-items-center mb-3 pb-1">
                           <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
-                          <span className="h1 fw-bold mb-0">Logo</span>
+                          <span className="h1 fw-bold mb-0 mx-auto">Sign into your account</span>
                         </div>
   
-                        <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>Sign into your account</h5>
+                        <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}></h5>
   
                         <div className="form-outline mb-4">
                           <input type="email" id="form2Example17" className="form-control form-control-lg" />
