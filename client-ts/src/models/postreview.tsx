4762d0576946d791a6eb14877078611adfcb1ff2
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-02-25 20:34:11 +0700

    comment func in process

--- client-ts/src/models/postreview.tsx
+++ client-ts/src/models/postreview.tsx
@@ -1,11 +1,10 @@
 export default interface Postreview {
     data: {
-        tour_id : number;
-        tour_name: string;
-        author: string;
-        score: number;
-        avatar : string;
-        comment : string;
+        // tour_id: number;
+        // tour_name: string;
+        // // author: string;
+        // score: number;
+        comment: string;
         date: string;
     }
 }
--- client-ts/src/page/Review.tsx
+++ client-ts/src/page/Review.tsx
@@ -1,14 +1,17 @@
-import UserNavbar from "../components/UserNavbar";
-import "../design/Review.css"
-import Repo from '../repositories'
-import Review from '../models/review';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import UserNavbar from "../components/UserNavbar";
 import ReviewSection from "../components/ReviewSection";
+import CommentSection from "../components/CommentSection";
+import Tours from "../models/tour";
+import Review from '../models/review';
+import Repo from '../repositories'
+import "../design/Review.css"
 
 
 const ReviewPage = () => {
     const [reviews, setReviews] = useState<Review[]>([]);
+    const [tourdata, setTourdata] = useState<Tours[]>([]);
     const params = useParams();
 
     const fetchData = async () => {
@@ -38,9 +41,8 @@ const ReviewPage = () => {
                                 <h6>Comments</h6>
                             </div>
 
-                            <div className="mt-3 d-flex flex-row align-items-center p-3 form-color gap-3">
-                                <img src="https://cdn.pixabay.com/photo/2016/04/22/04/57/graduation-1345143_960_720.png" width="50" className="rounded-circle mr-2" />
-                                <input type="text" className="form-control" placeholder="Enter your comment..." />
+                            <div>
+                                <CommentSection tourdata={tourdata[0]} />
                             </div>
 
                             <div>
