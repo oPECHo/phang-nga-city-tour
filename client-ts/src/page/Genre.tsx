6d2fc7e36a11c462dcc5271c1d17468155d3feb1
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-02-25 10:07:51 +0700

    edit layout

--- client-ts/src/page/Genre.tsx
+++ client-ts/src/page/Genre.tsx
@@ -10,54 +10,55 @@ import TripCard from '../components/tripCard';
 
 
 const GenrePage = () => {
-  const [data, setTourData] = useState<Tour[]>([]);
-  const params = useParams()
+    const [data, setTourData] = useState<Tour[]>([]);
+    const params = useParams()
 
-  const fetchData = async () => {
-      const res = await Repo.Tourdata.getCategory(params.type as string)
-      if(res) {
-          setTourData(res)
-      }
-  }
+    const fetchData = async () => {
+        const res = await Repo.Tourdata.getCategory(params.type as string)
+        if(res) {
+            setTourData(res)
+        }
+    }
 
-  useEffect(() => {
-      fetchData()
-  }, [params.type])
-  const headtitle = params.type==="One day trip" ? "One Day Trip" : "Package"
+    useEffect(() => {
+        fetchData()
+    }, [params.type])
+    const headtitle = params.type==="One day trip" ? "One Day Trip" : "Package"
 
 
-  return (
-      <div>
-          <UserNavbar/>
-          <Container>
-              <Row style={{ marginTop: "20px", right:"500px" }}>
-                  <Col md={{ span: 6, offset: 4 }} style={{ display: "flex" }}>
-                  </Col>
-              </Row>
-              <Row style={{ marginBottom: '15px'}}>
-                  <Col xs={12} md={3} 
-                      style={{ 
-                          backgroundColor: "whitesmoke", 
-                          borderRadius: "12px", 
-                          padding: "10px", 
-                          fontSize: '17px',
-                          fontWeight: "normal",
-                          textAlign: "center",
-                          }}>
-                      {headtitle}
-                  </Col>
-              </Row>
-              <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10}}>
-                {data.map((item, index) => 
-                    <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-                        <CardTour Tours={item} />
-                    </Grid>
-                )}
-              </Grid>
-          </Container>
-          <div className="container py-5 h-100"><TripCard/></div>
-      </div>
+    return (
+        <div>
+            <UserNavbar/>
+            <Container>
+                <Row style={{ marginTop: "20px", right:"500px" }}>
+                    <Col md={{ span: 6, offset: 4 }} style={{ display: "flex" }}>
+                    </Col>
+                </Row>
+                <Row style={{ marginBottom: '15px'}}>
+                    <Col xs={12} md={3} 
+                        style={{ 
+                            backgroundColor: "whitesmoke", 
+                            borderRadius: "12px", 
+                            padding: "10px", 
+                            fontSize: '17px',
+                            fontWeight: "normal",
+                            textAlign: "center",
+                            }}>
+                        {headtitle}
+                    </Col>
+                </Row>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 4, md: 4, lg: 4, xl: 4 }}>
+                    {data.map((item, index) => 
+                        <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
+                            <CardTour Tours={item} />
+                        </Grid>
+                    )}
+                </Grid>
+            </Container>
+            <div className="container py-5 h-100"><TripCard/></div>
+        </div>
 
-  )
-}
+    )
+};
+
 export default GenrePage;
