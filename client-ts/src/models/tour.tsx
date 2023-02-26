381f3499d6fcc5b6667fff117925d5380bdfa1e4
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-02-26 22:21:01 +0700

    change Tours interface layout

--- client-ts/src/models/tour.tsx
+++ client-ts/src/models/tour.tsx
@@ -1,42 +1,43 @@
 import { ReactNode } from "react";
 
-export default interface Tours{
+export default interface Tours {
     id: number;
     attributes: {
-        available_seat: ReactNode;
+        available_seat: number;
         title: string;
         price: number;
         address: string;
         number: number;
         score: number;
         description: string;
-        conditions: string ;
+        conditions: string;
         image: {
             data: {
                 attributes: {
                     formats: {
                         thumbnail: {
                             url: string;
-                        }
-                    }
-                }
-            }
-        }
+                        };
+                    };
+                };
+            };
+        };
         category: {
             data: {
                 attributes: {
-                    Type: string;
-                }
-            }
-        }
-        tour_date : {
-            data : {
+                    type: string;
+                };
+            };
+        };
+        tour_date: {
+            data: {
                 attributes: {
-                    first_trip : string;
-                    second_trip : string;
-                    first_trip_end : string | null;
-                    second_trip_end : string | null;
-                }
-            } | null
-        }
-}}
+                    first_trip: string;
+                    second_trip: string;
+                    first_trip_end: string | null;
+                    second_trip_end: string | null;
+                };
+            } | null;
+        };
+    };
+}
