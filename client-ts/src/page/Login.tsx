4e4e86fb71021d31ef1cecdd88355032c756241d
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-02-28 14:42:01 +0700

    unknown edit

--- client-ts/src/page/Login.tsx
+++ client-ts/src/page/Login.tsx
@@ -79,28 +79,27 @@ export default function Login() {
                     <div className="card-body p-4 p-lg-5 text-black">
 
                       <form>
-
-                        <div className="d-flex align-items-center mb-3 pb-1">
-                          <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
-                          <span className="h1 fw-bold mb-0 mx-auto">Sign into your account</span>
-                        </div>
+                          <div className="d-flex align-items-center mb-3 pb-1">
+                            <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                            <span className="h1 fw-bold mb-0 mx-auto">Sign into your account</span>
+                          </div>
 
                         <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}></h5>
 
-                        <div className="form-outline mb-4">
-                          <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                          <label className="form-label" htmlFor="form2Example17">Email address</label>
-                        </div>
+                          <div className="form-outline mb-4">
+                            <input type="email" id="form2Example17" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form2Example17">Email address</label>
+                          </div>
 
-                        <div className="form-outline mb-4">
-                          <input type="password" id="form2Example27" className="form-control form-control-lg" />
-                          <label className="form-label" htmlFor="form2Example27">Password</label>
-                        </div>
+                          <div className="form-outline mb-4">
+                            <input type="password" id="form2Example27" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form2Example27">Password</label>
+                          </div>
 
                         <div className="pt-1 mb-4">
                           <button className="btn btn-dark btn-lg btn-block" type="button">Login</button>
                         </div>
-
+                        
                         <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="/Register"
                           style={{ color: '#393f81' }}>Register here</a></p>
                       </form>
