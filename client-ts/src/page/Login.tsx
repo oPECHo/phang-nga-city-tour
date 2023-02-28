23feb05a84711502684019129587a57aa05a6776
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-02-28 15:30:31 +0700

    edit z-axis

--- client-ts/src/page/Login.tsx
+++ client-ts/src/page/Login.tsx
@@ -56,7 +56,7 @@ export default function Login() {
   return (
     <div className='Login-BG'>
       <UserNavbar />
-      <section className="vh-100 fixed-top">
+      <section className="vh-100 fixed-top" style={{ zIndex: '100' }}>
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col col-xl-10">
