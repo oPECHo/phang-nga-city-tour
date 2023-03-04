52f35068b2335fa4a487023d81036db49f30bc5b
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-03-04 15:47:22 +0700

    add local search system

--- client-ts/src/components/SearchBar.tsx
+++ client-ts/src/components/SearchBar.tsx
@@ -2,33 +2,39 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const SearchBar = () => {
+interface SearchBarProps {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBar = ({ onChange }: SearchBarProps) => {
   return (
-    <div className="container mt-3">
-      <div className="row">
+    <div className="container mt-5 mb-5">
+      <div className="row justify-content-between">
         <div className="col-md-6">
           <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={onChange} onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                event.preventDefault();
+              }
+            }} />
           </form>
         </div>
-        <div className="col-md-6">
-          <div className="text-end">
-            <div className="dropdown">
-              <button className="btn btn-danger dropdown-toggle"
-                      type="button"
-                      id="dropdownMenuButton"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                      style={{marginTop: '10px'}}>
-                <span>หมวดหมู่</span>
-                <i className="fa fa-caret-down"></i>
-              </button>
-              <ul className="dropdown-menu dropdown-menu-end" id="dropdownMenu" data-bs-popper="static">
-                <li><a className="dropdown-item" href="/Genre/All">ทั้งหมด</a></li>
-                <li><a className="dropdown-item" href="/Genre/One%20day%20trip">ทริปวันเดียว</a></li>
-                <li><a className="dropdown-item" href="/Genre/Package">แพ็คเกจ</a></li>
-              </ul>
-            </div>
+        <div className="col-md-6 text-end">
+          <div className="dropdown">
+            <button className="btn btn-danger dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              style={{ marginTop: '10px' }}>
+              <span>หมวดหมู่</span>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <ul className="dropdown-menu dropdown-menu-end" id="dropdownMenu" data-bs-popper="static">
+              <li><a className="dropdown-item" href="/Genre/All">ทั้งหมด</a></li>
+              <li><a className="dropdown-item" href="/Genre/One%20day%20trip">ทริปวันเดียว</a></li>
+              <li><a className="dropdown-item" href="/Genre/Package">แพ็คเกจ</a></li>
+            </ul>
           </div>
         </div>
       </div>
