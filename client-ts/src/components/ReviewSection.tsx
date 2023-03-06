18b31c5940d128bcd3f2c266d32a3130bf8d7a2a
นกฤช กรีไชยชนะ <kreechaichana3@gmail.com>
2023-03-07 02:17:18 +0700

    remove tourName behind username

--- client-ts/src/components/ReviewSection.tsx
+++ client-ts/src/components/ReviewSection.tsx
@@ -12,28 +12,27 @@ function ReviewSection(props: Props) {
         <div>
             <div className="mt-1">
                 <div className="d-flex flex-row p-3 gap-3">
-                <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                    width="40"
-                    height="40"
-                    className="rounded-circle mr-3"
-                />
-                <div className="w-100">
-                    <div className="d-flex justify-content-between align-items-center">
-                    <div className="d-flex flex-row align-items-center gap-1" >
-                        <span className="mr-2 fw-bold">{reviewData?.user}</span>
-                        <Rating name="read-only mx-auto" value={reviewData?.score} size="small" readOnly />
-                        <span className="ms-1 fw-light">({reviewData?.name_tour})</span>
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                        width="40"
+                        height="40"
+                        className="rounded-circle mr-3"
+                    />
+                    <div className="w-100">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="d-flex flex-row align-items-center gap-1" >
+                                <span className="mr-2 fw-bold">{reviewData?.user}</span>
+                                <Rating name="read-only mx-auto" value={reviewData?.score} size="small" readOnly />
+                            </div>
+                            <small>{reviewData?.Date}</small>
+                        </div>
+                        <p className="text-justify comment-text mb-0">{reviewData?.comment}</p>
+                        <div className="d-flex flex-row user-feed"></div>
                     </div>
-                    <small>{reviewData?.Date}</small>
-                    </div>
-                    <p className="text-justify comment-text mb-0">{reviewData?.comment}</p>
-                    <div className="d-flex flex-row user-feed"></div>
-                </div>
                 </div>
             </div>
         </div>
-        );
-    };
+    );
+};
 
 export default ReviewSection;
